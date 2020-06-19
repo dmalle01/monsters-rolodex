511545c23edeb0e88660aed6be70f6efcb81bd89
Deepa Malleeswaran <deepamalleeswaran@Deepas-MBP.fios-router.home>
2020-06-19 10:46:41 -0400

    Updates

---
+++ precache-manifest.5ed8a422cfa6599d70130521f8e31e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54b83d4de6582f72d6573dadc61a335",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e48f201d96901ab3a12",
+    "url": "/monsters-rolodex/static/css/main.42fdea05.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e48f201d96901ab3a12",
+    "url": "/monsters-rolodex/static/js/main.fe19c193.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
